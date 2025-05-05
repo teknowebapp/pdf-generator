9067dd148614ba1c4e9a584a39aae30af336c44a
Gagas Sangga Pratama <49256437+haiigas@users.noreply.github.com>
2025-05-05 14:49:52 +0700

    Update and rename pdf_ci3.sql to pdf_generator.sql

--- database/pdf_generator.sql
+++ database/pdf_generator.sql
@@ -18,30 +18,30 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `pdf_ci3`
+-- Database: `pdf_generator`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_matkul`
+-- Table structure for table `lessons`
 --
 
-CREATE TABLE `tbl_matkul` (
+CREATE TABLE `lessons` (
   `id` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
-  `kode_matkul` varchar(30) NOT NULL,
-  `nama_matkul` varchar(80) NOT NULL,
-  `semester` int(11) NOT NULL,
+  `student` int(11) NOT NULL,
+  `code` varchar(30) NOT NULL,
+  `name` varchar(80) NOT NULL,
+  `periode` int(11) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_matkul`
+-- Dumping data for table `lessons`
 --
 
-INSERT INTO `tbl_matkul` (`id`, `id_user`, `kode_matkul`, `nama_matkul`, `semester`, `created_at`, `updated_at`) VALUES
+INSERT INTO `lessons` (`id`, `student`, `code`, `name`, `periode`, `created_at`, `updated_at`) VALUES
 (1, 1, 'TIF001', 'Pemrograman Dasar', 1, '2021-03-03 10:00:51', '2021-03-03 10:00:51'),
 (2, 1, 'TIF002', 'Pemrograman Mobile', 1, '2021-03-03 10:01:14', '2021-03-03 10:01:14'),
 (3, 1, 'TIF003', 'Jaringan Komputer', 1, '2021-03-03 10:01:14', '2021-03-03 10:01:14'),
@@ -50,21 +50,21 @@ INSERT INTO `tbl_matkul` (`id`, `id_user`, `kode_matkul`, `nama_matkul`, `semest
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tbl_users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `tbl_users` (
+CREATE TABLE `users` (
   `id` int(11) NOT NULL,
-  `nama` varchar(80) NOT NULL,
+  `name` varchar(80) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp(),
   `updated_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `tbl_users`
+-- Dumping data for table `users`
 --
 
-INSERT INTO `tbl_users` (`id`, `nama`, `created_at`, `updated_at`) VALUES
+INSERT INTO `users` (`id`, `name`, `created_at`, `updated_at`) VALUES
 (1, 'Gagas Sangga Pratama', '2021-03-03 09:59:32', '2021-03-03 09:59:32');
 
 --
@@ -72,16 +72,16 @@ INSERT INTO `tbl_users` (`id`, `nama`, `created_at`, `updated_at`) VALUES
 --
 
 --
--- Indexes for table `tbl_matkul`
+-- Indexes for table `lessons`
 --
-ALTER TABLE `tbl_matkul`
+ALTER TABLE `lessons`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_user` (`id_user`);
+  ADD KEY `student` (`student`);
 
 --
--- Indexes for table `tbl_users`
+-- Indexes for table `users`
 --
-ALTER TABLE `tbl_users`
+ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -89,15 +89,15 @@ ALTER TABLE `tbl_users`
 --
 
 --
--- AUTO_INCREMENT for table `tbl_matkul`
+-- AUTO_INCREMENT for table `lessons`
 --
-ALTER TABLE `tbl_matkul`
+ALTER TABLE `lessons`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `tbl_users`
+-- AUTO_INCREMENT for table `users`
 --
-ALTER TABLE `tbl_users`
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
