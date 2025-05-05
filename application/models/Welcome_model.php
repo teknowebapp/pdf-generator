98317b4f4e8ce26e1c960c8223bc9450338fbf21
Gagas Sangga Pratama <49256437+haiigas@users.noreply.github.com>
2025-05-05 14:45:41 +0700

    Update Welcome_model.php

--- application/models/Welcome_model.php
+++ application/models/Welcome_model.php
@@ -10,6 +10,6 @@ class Welcome_model extends CI_Model
 
     function get_print_course($student, $periode)
     {
-        return $this->db->query("SELECT lessons.id, lessons.code, lessons.name lesson, lessons.periode, users.name student FROM lessons JOIN users ON lessons.student = users.id WHERE lessons.student = '$student' AND lessons.periode = '$periode' ORDER BY lessons.id DESC");
+        return $this->db->query("SELECT lessons.id, lessons.code, lessons.name lesson, lessons.periode, users.name FROM lessons JOIN users ON lessons.student = users.id WHERE lessons.student = '$student' AND lessons.periode = '$periode' ORDER BY lessons.id DESC");
     }
 }
