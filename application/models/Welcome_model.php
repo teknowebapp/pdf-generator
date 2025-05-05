d8a705fa9ba83568ded14c72ce240fb27cf0767a
Gagas Sangga Pratama <49256437+haiigas@users.noreply.github.com>
2025-05-05 14:44:30 +0700

    Update Welcome_model.php

--- application/models/Welcome_model.php
+++ application/models/Welcome_model.php
@@ -3,13 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome_model extends CI_Model
 {
-    function get_cetak_matkul($id, $smt)
+    function get_course()
     {
-        return $this->db->query("SELECT a.*, b.nama as mahasiswa FROM tbl_matkul AS a JOIN tbl_users AS b ON a.id_user = b.id WHERE a.id_user = '$id' AND a.semester = '$smt' ORDER BY a.id DESC");
+        return $this->db->query("SELECT lessons.id, lessons.code, lessons.name lesson, lessons.periode, users.name student FROM lessons JOIN users ON lessons.student = users.id ORDER BY lessons.id DESC")->result();
     }
 
-    function get_matkul()
+    function get_print_course($student, $periode)
     {
-        return $this->db->query("SELECT a.*, b.nama as mahasiswa FROM tbl_matkul AS a JOIN tbl_users AS b ON a.id_user = b.id ORDER BY a.id DESC")->result();
+        return $this->db->query("SELECT lessons.id, lessons.code, lessons.name lesson, lessons.periode, users.name student FROM lessons JOIN users ON lessons.student = users.id WHERE lessons.student = '$student' AND lessons.periode = '$periode' ORDER BY lessons.id DESC");
     }
 }
