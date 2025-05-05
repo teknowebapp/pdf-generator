efca525ae0d33bd13d8382bc6e00ad626ea1e086
Gagas Sangga Pratama <49256437+haiigas@users.noreply.github.com>
2025-05-05 14:06:14 +0700

    Update Generatepdf.php

--- application/libraries/Generatepdf.php
+++ application/libraries/Generatepdf.php
@@ -6,25 +6,32 @@ use Dompdf\Exception;
 
 class Generatepdf
 {
-
-    public function generate($html, $filename="", $stream=TRUE, $paper = 'A4', $orientation = "portrait")
+    public function generate($html, $filename = "", $stream = true, $paper = 'A4', $orientation = "portrait")
     {
         define('DOMPDF_ENABLE_AUTOLOAD', false);
+
         $dompdf = new DOMPDF();
         $dompdf->loadHtml($html);
         $dompdf->setPaper($paper, $orientation);
         $dompdf->render();
-        if ($stream) { $dompdf->stream($filename, array("Attachment" => 0)); } else { return $dompdf->output(); }
+
+        if ($stream) {
+            $dompdf->stream($filename, array("Attachment" => 0));
+        } else {
+            return $dompdf->output();
+        }
     }
 
     public function save($html, $filename="", $paper = 'A4', $orientation = "portrait")
     {
         define('DOMPDF_ENABLE_AUTOLOAD', false);
+
         $dompdf = new DOMPDF();
         $dompdf->load_html($html);
         $dompdf->setPaper($paper, $orientation);
         $dompdf->render();
         $output = $dompdf->output();
-        file_put_contents($_SERVER["DOCUMENT_ROOT"].'/pdf/assets/pdf/'.$filename, $output);
+
+        file_put_contents($_SERVER["DOCUMENT_ROOT"] . '/pdf/assets/pdf/' . $filename, $output);
     }
 }
